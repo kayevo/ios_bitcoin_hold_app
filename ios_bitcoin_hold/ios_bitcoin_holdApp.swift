68f8bc82eefa78a0ed41e4065cfd61cc2287a1e1
Kayevo <kayevo@protonmail.com>
2023-06-17 20:00:13 -0300

    init: xcode project

---
+++ ios_bitcoin_hold/ios_bitcoin_holdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_bitcoin_holdApp.swift
+//  ios_bitcoin_hold
+//
+//  Created by Natan Kayevo on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_bitcoin_holdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
