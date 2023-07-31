f5d85bd595bef2c575277fa02f893b30baaa7a93
Kayevo <kayevo@protonmail.com>
2023-07-31 20:49:16 -0300

    style: add sign in page style

--- ios_bitcoin_hold/SignInView.swift
+++ ios_bitcoin_hold/SignInView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 import Combine
 
+class TextValidator: ObservableObject {
+
+    @Published var text = ""
+
+}
+
 struct SignInView: View {
     @State private var email = ""
     @State private var password = ""
@@ -8,49 +14,71 @@ struct SignInView: View {
     @State private var wrongPassword = ""
     @State private var logged = false
     @StateObject private var signInViewModel = SignInViewModel(userService: MockSignInServiceImpl())
-        
+    @StateObject private var textValidator = TextValidator()
+    
+    
     var body: some View {
+        
         NavigationView{
             ZStack{
-                Color.gray
+                let primaryDarkBlue = UIColor(red: 0.16, green: 0.19, blue: 0.24, alpha: 1)
+                let primaryLigthBlue = UIColor(red: 0.2, green: 0.24, blue: 0.29, alpha: 1)
+                let primaryGreen = UIColor(red: 0.1, green: 0.77, blue: 0.51, alpha: 1)
+                Color(uiColor: primaryLigthBlue)
                     .ignoresSafeArea()
                 VStack{
                     Text("Bitcoin Hold")
                         .font(.largeTitle)
+                        .foregroundColor(Color(white: 1))
                     TextField("E-mail", text: $email)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background()
                         .cornerRadius(10)
-                    SecureField("Password", text: $password)
+                    /*
+                    TextField("E-mail", text: $textValidator.text)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background()
                         .cornerRadius(10)
-                    Button("SignIn"){
+                        .onReceive(Just(textValidator.text)){ newValue in
+                            if(newValue)
+                            
+                        }
+                    
+                     Text("E-mail validator")
+                        .font(.title3)
+                        .foregroundColor(Color(white: 1))
+                     */
+                     SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background()
+                        .cornerRadius(10)
+                    /*Text("Password validator")
+                        .font(.title3)
+                        .foregroundColor(Color(white: 1))
+                    */
+                     Button("SignIn"){
                         signInViewModel.signIn(email: email, password: password)
                         logged = signInViewModel.userSignIn
                     }
                     .frame(width: 300, height: 50)
-                    .background(Color.black)
-                    .foregroundColor(Color.white)
+                    .background(Color(primaryGreen))
+                    .foregroundColor(Color(primaryLigthBlue))
                     .cornerRadius(10)
+                    .padding(.top, 20)
                     
                     NavigationLink(destination: Text("Logged in"), isActive: $logged){
                         EmptyView()
                     }
-                    
                 }
             }
-            .navigationBarHidden(true)
         }
-    }
-    func authenticateUser(username: String, password: String) -> Bool{
-        return true
     }
 }
 
-struct ContentyouView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         SignInView()
     }
--- ios_bitcoin_hold/service/impl/MockSignInServiceImpl.swift
+++ ios_bitcoin_hold/service/impl/MockSignInServiceImpl.swift
@@ -3,7 +3,6 @@ import Combine
 
 class MockSignInServiceImpl : SignInService, ObservableObject{
     func signIn(credential: UserCredential)-> AnyPublisher<Bool, Error>{
-        print("User saved.")
         let publisher = Just(true).setFailureType(to: Error.self).eraseToAnyPublisher()
         return publisher
     }
