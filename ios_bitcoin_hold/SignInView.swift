07a6bda9e3c42918cc8909761aff44fefe1884ca
Kayevo <kayevo@protonmail.com>
2023-10-08 18:33:55 -0300

    feat: add login form

--- ios_bitcoin_hold/SignInView.swift
+++ ios_bitcoin_hold/SignInView.swift
@@ -9,16 +9,35 @@ class TextValidator: ObservableObject {
 
 struct SignInView: View {
     @State private var email = ""
+    @State private var hintEmail = ""
     @State private var password = ""
-    @State private var wrongUsername = ""
-    @State private var wrongPassword = ""
+    @State private var hintPassword = ""
     @State private var logged = false
     @StateObject private var signInViewModel = SignInViewModel(signInService: MockSignInServiceImpl())
     @StateObject private var textValidator = TextValidator()
+    let primaryDarkBlue = UIColor(red: 0.16, green: 0.19, blue: 0.24, alpha: 1)
+    let primaryLightBlue = UIColor(red: 0.2, green: 0.24, blue: 0.29, alpha: 1)
+    let primaryGreen = UIColor(red: 0.1, green: 0.77, blue: 0.51, alpha: 1)
     
+    init() {
+        //Use this if NavigationBarTitle is with Large Font
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: primaryGreen]
+    }
     
     var body: some View {
-        
+        NavigationView{
+            ZStack {
+                Color(primaryLightBlue)
+                    .ignoresSafeArea()
+                
+                Form {
+                    TextField("E-mail", text: $email)
+                }
+                .scrollContentBackground(.hidden)
+                .navigationTitle("Login")
+            }
+        }
+        /*
         NavigationView{
             ZStack{
                 let primaryDarkBlue = UIColor(red: 0.16, green: 0.19, blue: 0.24, alpha: 1)
@@ -35,6 +54,14 @@ struct SignInView: View {
                         .frame(width: 300, height: 50)
                         .background()
                         .cornerRadius(10)
+                        .onChange(of: email) { newValue in
+                            // This function will be called every time the 'email' variable changes
+                            // You can perform your desired actions here
+                            print("Text typed: \(newValue)")
+                        }
+                    Text("Invalid e-mail")
+                        .font(.title3)
+                        .foregroundColor(Color(white: 1))
                     SecureField("Password", text: $password)
                         .padding()
                         .frame(width: 300, height: 50)
@@ -56,6 +83,7 @@ struct SignInView: View {
                 }
             }
         }
+    */
     }
 }
 
