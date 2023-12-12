a0b83347514f484be395457bda6617b82691eddd
Kayevo <kayevo@protonmail.com>
2023-12-12 19:33:03 -0300

    feat: add menu donations and logout actions on porfolio

--- ios_bitcoin_hold/ios_bitcoin_holdApp.swift
+++ ios_bitcoin_hold/ios_bitcoin_holdApp.swift
@@ -3,7 +3,8 @@ import SwiftUI
 @main
 struct ios_bitcoin_holdApp: App {
     var body: some Scene {
-        WindowGroup {            
+        WindowGroup {     
+            // TODO insert sign in view
             SignInView()
         }
     }
--- ios_bitcoin_hold/view/PortfolioView.swift
+++ ios_bitcoin_hold/view/PortfolioView.swift
@@ -13,111 +13,134 @@ struct PortfolioView: View {
     @State var profits: String = "loading..."
     @StateObject var portfolioViewModel: PortfolioViewModel
     private var cancellables = Set<AnyCancellable>()
+    let secretDictionary = NSDictionary(
+        contentsOfFile: Bundle.main.path(forResource: "Secret", ofType: "plist") ?? ""
+    )
+    let supportEmail: String
+    @State var displaySupportEmail = false
+    @State var isLoggedIn = true
     
     init(userId: String){
+        
         self.userId = userId
         self._portfolioViewModel = StateObject(wrappedValue: PortfolioViewModel(
             portfolioService: PortfolioServiceImpl(),
             analysisService: AnalysisServiceImpl(),
             userId: userId
         ))
+        self.supportEmail = (secretDictionary?["SUPPORT_EMAIL"] as? String) ?? ""
     }
     
     var body: some View {
-        NavigationStack {
-            VStack{
-                Spacer()
-                Text("Portfolio")
-                    .foregroundColor(Color(primaryGreen))
-                    .font(.title)
-                    .toolbar {
-                        ToolbarItem(placement: .primaryAction) {
-                            Menu {
-                                NavigationLink(destination: {
-                                    DonationView()
-                                }, label: {
-                                    Label("Donations", systemImage: "creditcard")
-                                })
-                                NavigationLink(destination: {
-                                    // TODO add customize view, dont do modal
-                                }, label: {
-                                    Label("Customize portfolio amount", systemImage: "square.and.pencil")
-                                })
-                                NavigationLink(destination: {
-                                    // TODO add support modal with button to copy email
-                                }, label: {
-                                    Label("Support", systemImage: "person.wave.2")
-                                })
-                                NavigationLink(destination: {
-                                    // TODO logout user
-                                }, label: {
-                                    Label("Logout", systemImage: "arrow.up.forward.app")
-                                })
+        if isLoggedIn{
+            
+            NavigationStack {
+                VStack{
+                    Spacer()
+                    Text("Portfolio")
+                        .foregroundColor(Color(primaryGreen))
+                        .font(.title)
+                        .toolbar {
+                            ToolbarItem(placement: .primaryAction) {
+                                Menu {
+                                    NavigationLink(destination: {
+                                        DonationView()
+                                    }, label: {
+                                        Label("Donations", systemImage: "creditcard")
+                                    })
+                                    NavigationLink(destination: {
+                                        // TODO add customize view, dont do modal
+                                    }, label: {
+                                        Label("Customize portfolio amount", systemImage: "square.and.pencil")
+                                    })
+                                    Button(action: {
+                                        displaySupportEmail = true
+                                    }, label: {
+                                        Label("Support", systemImage: "person.wave.2")
+                                    })
+                                    Button(action: {
+                                        isLoggedIn = false
+                                    }, label: {
+                                        Label("Logout", systemImage: "arrow.up.forward.app")
+                                    })
+                                }
+                            label: {
+                                VStack{
+                                    Image(systemName: "slider.horizontal.3")
+                                    Text("Menu").foregroundColor(.white)
+                                }
                             }
-                        label: {
-                            VStack{
-                                Image(systemName: "slider.horizontal.3")
-                                Text("Menu").foregroundColor(.white)
                             }
                         }
-                        }
+                    HStack {
+                        Text("Bitcoins:")
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text(bitcoins)
+                            .foregroundColor(.white)
                     }
-                HStack {
-                    Text("Bitcoins:")
-                        .foregroundColor(.white)
+                    HStack {
+                        Text("Average price:")
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text(averagePrice)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.bottom, 60)
+                    Text("Analysis")
+                        .foregroundColor(Color(primaryGreen))
+                        .font(.title)
+                    HStack {
+                        Text("Bitcoin price:")
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text(bitcoinPrice)
+                            .foregroundColor(.white)
+                    }
+                    HStack {
+                        Text("Portfolio value:")
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text(portfolioValue)
+                            .foregroundColor(.white)
+                    }
+                    HStack {
+                        Text("Profits:")
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text(profits)
+                            .foregroundColor(.white)
+                    }
                     Spacer()
-                    Text(bitcoins)
-                        .foregroundColor(.white)
                 }
-                HStack {
-                    Text("Average price:")
-                        .foregroundColor(.white)
-                    Spacer()
-                    Text(averagePrice)
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 60)
-                Text("Analysis")
-                    .foregroundColor(Color(primaryGreen))
-                    .font(.title)
-                HStack {
-                    Text("Bitcoin price:")
-                        .foregroundColor(.white)
-                    Spacer()
-                    Text(bitcoinPrice)
-                        .foregroundColor(.white)
-                }
-                HStack {
-                    Text("Portfolio value:")
-                        .foregroundColor(.white)
-                    Spacer()
-                    Text(portfolioValue)
-                        .foregroundColor(.white)
-                }
-                HStack {
-                    Text("Profits:")
-                        .foregroundColor(.white)
-                    Spacer()
-                    Text(profits)
-                        .foregroundColor(.white)
-                }
-                Spacer()
+                .padding(.horizontal, 50)
+                .padding(20)
+                .background(Color(primaryDarkBlue))
+                .onReceive(portfolioViewModel.$analysis
+                    .compactMap { $0 }, perform: {analysis in
+                        self.bitcoinPrice = "\(analysis.bitcoinPriceInBrl.parseToCurrency())"
+                        self.portfolioValue = "\(analysis.portfolioValue.parseToCurrency())"
+                        self.profits = "\(analysis.profits.parseToPercentage())"
+                    }
+                )
+                .onReceive(portfolioViewModel.$portfolioAmount
+                    .compactMap { $0 }, perform: {portfolioAmount in
+                        self.bitcoins = "\(portfolioAmount.satoshiAmount.parseSatoshiToBitcoin())"
+                        self.averagePrice = "\(portfolioAmount.bitcoinAveragePrice.parseToCurrency())"
+                    })
+                .alert(
+                    "Support email",
+                    isPresented: $displaySupportEmail,
+                    actions: {
+                        Button("Ok", role: .cancel) {}
+                    },
+                    message: {
+                        Text("\(supportEmail)")
+                    }
+                )
             }
-            .padding(.horizontal, 50)
-            .padding(20)
-            .background(Color(primaryDarkBlue))
-            .onReceive(portfolioViewModel.$analysis
-                .compactMap { $0 }, perform: {analysis in
-                    self.bitcoinPrice = "\(analysis.bitcoinPriceInBrl.parseToCurrency())"
-                    self.portfolioValue = "\(analysis.portfolioValue.parseToCurrency())"
-                    self.profits = "\(analysis.profits.parseToPercentage())"
-                }
-            )
-            .onReceive(portfolioViewModel.$portfolioAmount
-                .compactMap { $0 }, perform: {portfolioAmount in
-                    self.bitcoins = "\(portfolioAmount.satoshiAmount.parseSatoshiToBitcoin())"
-                    self.averagePrice = "\(portfolioAmount.bitcoinAveragePrice.parseToCurrency())"
-                })
+        }else{
+            SignInView()
         }
     }
 }
