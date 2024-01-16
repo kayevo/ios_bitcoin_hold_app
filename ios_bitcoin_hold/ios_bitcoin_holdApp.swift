23ac625196e5f6f595a9e44ae5ccf93a533885ca
Kayevo <kayevo@protonmail.com>
2024-01-16 16:50:52 -0300

    refac: init in signin screen

--- ios_bitcoin_hold/ios_bitcoin_holdApp.swift
+++ ios_bitcoin_hold/ios_bitcoin_holdApp.swift
@@ -5,9 +5,7 @@ struct ios_bitcoin_holdApp: App {
     var body: some Scene {
         WindowGroup {     
             // TODO insert sign in view
-            PortfolioView( userId: 
-                (secretDictionary?["MOCK_USER_ID"] as? String) ?? ""
-            )
+            SignInView()
         }
     }
 }
